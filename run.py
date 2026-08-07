54bec215598ad3a8f8502bff13373e90ae2c0e07
Hariprakash-23 <kahariprakash23@lpu.in>
2026-08-07 15:56:11 +0530

    Initial commit5

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
 from app import create_app
-import os
+
+from config import Config
 
 app = create_app()
 
 if __name__ == "__main__":
+
     app.run(
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 5000)),
-        debug=False
-    )
+    host=Config.FLASK_HOST,
+    port=Config.FLASK_PORT,
+    debug=True,
+    use_reloader=False,
+    threaded=True
+)
